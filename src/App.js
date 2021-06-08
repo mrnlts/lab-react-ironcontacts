65ad84205b588552c882ebaf5ee7437788abcbe3
marina <marinaaltesmagret@gmail.com>
2021-06-08 11:34:17 +0200

    Iteration 3

--- src/App.js
+++ src/App.js
@@ -18,24 +18,42 @@ class App extends Component{
     })    
   }
   
+  sortByName = () => {
+    const compare = (a, b) => (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
+    this.setState({
+      initialContacts: this.state.initialContacts.sort(compare)
+    })
+  }
+
+  sortByPopularity = () => {
+    const compare = (a, b) => (a.popularity > b.popularity) ? -1 : (a.popularity < b.popularity) ? 1 : 0;
+    this.setState({
+      initialContacts: this.state.initialContacts.sort(compare)
+    })
+  }
+
   render() {
     const { initialContacts } = this.state;
     return (
       <div className="App">
       <h1>IronContacts</h1>
       <button onClick={this.addRandom}>Add Random Contact</button>
+      <button onClick={this.sortByName}>Sort by name</button>
+      <button onClick={this.sortByPopularity}>Sort by popularity</button>
       <table className="contactsTable">
         <tr>
           <th>Picture</th>
           <th>Name</th>
           <th>Popularity</th>
         </tr>
-        {initialContacts.map((contact, index) => <tr key={index}>
-          <td><img className="contactImage" src={contact.pictureUrl} alt={ contact.name}/></td>
-          <td>{contact.name}</td>
-          <td>{contact.popularity.toFixed(2)}</td>
-        </tr>)}
-      </table>
+        {initialContacts.map((contact, index) =>
+          <tr key={index}>
+            <td><img className="contactImage" src={contact.pictureUrl} alt={ contact.name}/></td>
+            <td>{contact.name}</td>
+            <td>{contact.popularity.toFixed(2)}</td>
+          </tr>
+        )}
+    </table>
     </div>
   )
 }
